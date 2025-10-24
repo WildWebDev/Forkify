416b96754b185c4cc65e7bcd4eb56942e44c2482
WildWebDev <239867263+WildWebDev@users.noreply.github.com>
2025-10-24 15:38:24 -0300

    🚀 Initial commit.

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+// export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '14f9bcd8-462e-4a43-8d7f-de898ec997b6';
+export const MODAL_CLOSE_SEC = 2.5;
